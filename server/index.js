a1ff81864237c2598eec2c744c880e4719259c69
ByungChan Lee <lbc2415@gmail.com>
2022-01-21 22:31:43 -0600

    added loader token verification purpose

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3000;
 const db = require('../database/index.js')
 const productRouter = require('./routes.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use('/products', productRouter);
+app.use('/', productRouter);
+
+app.get(`/${process.env.loader_token}`, (req, res) => {
+  res.status(200).send(process.env.loader_token)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
